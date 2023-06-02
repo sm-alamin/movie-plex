08dfcc5be3ea696634e358bae19d9634096363e3
sm-alamin <sma.fkml@gmail.com>
2023-06-02 07:23:13 +0600

    initial commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Hello from movie plex</h3>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h3>All right reserved by Movie Plex</h3>
+        </div>
+    );
+};
+
+export default Footer;
